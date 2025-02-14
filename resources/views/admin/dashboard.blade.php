113acd5964007e513842c2eb983a09d8cb3a0e6d
allaboutei <khantsithu.phoenix.kst@gmail.com>
2025-02-15 01:21:34 +0630

    Finishing Touch

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -6,9 +6,34 @@
             @include('admin.shared.left-sidebar')
         </div>
         <div class="col-9">
-<h1>Admin Dashboard</h1>
+            <h1>Admin Dashboard</h1>
+
+            <div class="row">
+                <div class="col-sm-6 col-md-4">
+                    @include('shared.widget',[
+                        'title' => 'Total Users',
+                        'icon' => 'fas fa-users',
+                        'data' => $totalUsers
+
+                    ])
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    @include('shared.widget',[
+                        'title' => 'Total Ideas',
+                        'icon' => 'fas fa-lightbulb',
+                        'data' => $totalIdeas
+
+                    ])
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    @include('shared.widget',[
+                        'title' => 'Total Comments',
+                        'icon' => 'fas fa-comment',
+                        'data' => $totalComments
+
+                    ])
+                </div>
+            </div>
 
         </div>
-
-    </div>
-@endsection
+    @endsection
---
+++ resources/views/shared/widget.blade.php
@@ -0,0 +1,8 @@
+<div class="card">
+    <div class="card-body">
+        <p class="fs-4" class="card-title"><span class="{{ $icon ?? 'fas fa-users' }}"></span>{{ $title ?? '' }}</p
+            class="fs-4">
+        <h2 class="">{{ $data ?? '' }}</h2>
+
+    </div>
+</div>
